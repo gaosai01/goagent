b2e1bd35c1ddadad29beb81e8739ec22a567e9b1
Mr-GaoSai <gaosaibike@163.com>
2018-08-04 17:47:57 +0800

    优化配置管理部分和docker

--- push.sh
+++ push.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # 编译
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o dist/linux
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o dist/window64.exe
-go build -o dist/macos
+#CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o dist/linux
+#CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o dist/window64.exe
+#go build -o dist/macos
 # 上传到git仓库
 git add .
 git commit -m "优化配置管理部分和docker"
